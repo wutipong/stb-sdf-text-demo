be688ae0fdcce0cdd4cac5b1e3c98d15ac99a4f7
Wutipong Wongsakuldej <wutipong@trueaxiongames.com>
2021-02-17 21:46:35 +0700

    update the user interface code.

--- main_scene.cpp
+++ main_scene.cpp
@@ -1,11 +1,38 @@
 #include "main_scene.hpp"
 
-#include <imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 void MainScene::Draw() {}
 
 void MainScene::DrawUI() {
-  ImGui::Begin("Hello");
-  ImGui::LabelText("Hello", "Hello World");
+  filebrowser.Display();
+  ImGui::Begin("Font File");
+  {
+    ImGui::LabelText("Font", fontFilePath.filename().string().c_str());
+
+    if (ImGui::Button("Select font")) {
+      filebrowser.SetTitle("Select font file.");
+      filebrowser.SetTypeFilters({".ttf", ".otf"});
+      filebrowser.Open();
+    }
+
+    if (filebrowser.HasSelected()) {
+      fontFilePath = filebrowser.GetSelected();
+      filebrowser.ClearSelected();
+    }
+
+    ImGui::SliderInt("Font Size", &size, 1, 256);
+    ImGui::ColorEdit3("Color", glm::value_ptr(color),
+                      ImGuiColorEditFlags_Float);
+  }
+  ImGui::End();
+
+  ImGui::Begin("Input Text");
+  {
+    std::array<char, 200> buffer{};
+    if (ImGui::InputTextMultiline("Input", buffer.data(), buffer.size())) {
+      text = std::string{buffer.begin(), buffer.end()};
+    };
+  }
   ImGui::End();
 }
--- main_scene.hpp
+++ main_scene.hpp
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <glm/glm.hpp>
+#include <imgui.h>
+#include <string>
+
+#include "imgui-filebrowser/imfilebrowser.h"
+
 class MainScene {
 public:
   void DrawUI();
   void Draw();
+
+private:
+  ImGui::FileBrowser filebrowser;
+  std::string text;
+  std::filesystem::path fontFilePath;
+  int size{64};
+  glm::vec3 color{};
 };
